82d905dad08ebcd3a7660b8b77b9ea36933c4464
athulspillai12334 <athulspillai2000@gmail.com>
2024-05-31 14:10:28 +0530

    updated

--- src/components/subcategory/Subcategory.js
+++ src/components/subcategory/Subcategory.js
@@ -1,9 +1,50 @@
-import React from 'react'
+import React from 'react';
 
-function Subcategory() {
-  return (
-    <div>Subcategory</div>
-  )
+// Function to check if a number is prime
+function isPrime(num) {
+    if (num <= 1) return false;
+    if (num <= 3) return true;
+
+    if (num % 2 === 0 || num % 3 === 0) return false;
+
+    for (let i = 5; i * i <= num; i += 6) {
+        if (num % i === 0 || num % (i + 2) === 0) return false;
+    }
+
+    return true;
 }
 
-export default Subcategory
+// Get all prime numbers from 1 to 10
+const primes = [];
+// Get all odd numbers from 1 to 10
+const odds = [];
+for (let i = 1; i <= 10; i++) {
+    if (isPrime(i)) {
+        primes.push(i);
+    }
+    if (i % 2 !== 0) {
+        odds.push(i);
+    }
+}
+
+function Subcategory() {
+    return (
+        <div>
+            <p style={{ fontSize: '0.5rem' }}>Prime Numbers from 1 to 10</p>
+            <ul>
+                {primes.map((prime, index) => (
+                    <li key={index}>{prime}</li>
+                ))}
+            </ul>
+            <h1  style={{ fontSize: '0.5rem' }}>Odd Numbers from 1 to 10</h1>
+            <ul>
+                {odds.map((odd, index) => (
+                    <li key={index}>{odd}</li>
+                ))}
+            </ul>
+        </div>
+    );
+}
+
+export default Subcategory;
+
